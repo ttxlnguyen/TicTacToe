e9a20ac6bf329710813a538d0cc0f001386c61cc
ttxlnguyen <ttxl.nguyen@gmail.com>
2024-08-01 17:49:56 -0400

    finished

--- src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
+++ src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
@@ -4,23 +4,124 @@ package rocks.zipcodewilmington.tictactoe;
  * @author leon on 6/22/18.
  */
 public class Board {
+
+    public Character[][] matrix;
+
     public Board(Character[][] matrix) {
+        this.matrix = matrix;
     }
 
     public Boolean isInFavorOfX() {
-        return null;
+        Boolean horizontalWinner = checkHorizontal('X');
+        Boolean verticalWinner = checkVertical('X');
+        Boolean diagForwards = checkDiagonalForwards('X');
+        Boolean diagBackwards = checkDiagonalBackwards('X');
+
+        if (horizontalWinner || verticalWinner || diagForwards || diagBackwards){
+            return true;
+        }
+        return false;
     }
 
     public Boolean isInFavorOfO() {
-        return null;
+        Boolean horizontalWinner = checkHorizontal('O');
+        Boolean verticalWinner = checkVertical('O');
+        Boolean diagForwards = checkDiagonalForwards('O');
+        Boolean diagBackwards = checkDiagonalBackwards('O');
+
+        if (horizontalWinner || verticalWinner || diagForwards || diagBackwards){
+            return true;
+        }
+        return false;
     }
 
     public Boolean isTie() {
-        return null;
+        if (isInFavorOfO() == isInFavorOfX()) {
+            return true;
+        }
+        return false;
     }
 
     public String getWinner() {
-        return null;
+        if (isTie()){
+            return "";
+        } else if (!isTie() && !isInFavorOfO() && isInFavorOfX()){
+            return "X";
+        } else if (!isTie() && !isInFavorOfX() && isInFavorOfO()){
+            return "O";
+        } else{
+            return null;
+        }
     }
 
+    public Boolean checkHorizontal(char player){
+        int comboCounter =0;
+        for (int i = 0; i< matrix.length; i++){
+            for (int j = 0; j < matrix.length; j++){
+                if (matrix[i][j] == player){
+                    comboCounter++;
+                } else {
+                    comboCounter = 0;
+                }
+            }
+            if (comboCounter == 3){
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    public Boolean checkVertical(char player){
+        int comboCounter =0;
+        for (int i = 0; i< matrix.length; i++){
+            for (int j = 0; j < matrix.length; j++){
+                if (matrix[j][i] == player){
+                    comboCounter++;
+                } else {
+                    comboCounter = 0;
+                }
+            }
+            if (comboCounter == 3){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Boolean checkDiagonalForwards (char player){
+        int comboCounter =0;
+        int i = 0;
+        for (int j = 0; j < matrix.length; j++){
+            if (matrix[j][i] == player){
+                comboCounter++;
+                i++;
+            } else {
+                comboCounter = 0;
+                i++;
+            }
+        }
+        if (comboCounter == 3){
+            return true;
+        }
+        return false;
+    }
+
+    public Boolean checkDiagonalBackwards (char player){
+        int comboCounter =0;
+        int i = 0;
+        for (int j = 2; j >= 0; j--){
+            if (matrix[i][j] == player){
+                comboCounter++;
+                i++;
+            } else {
+                comboCounter = 0;
+                i++;
+            }
+        }
+        if (comboCounter == 3){
+            return true;
+        }
+        return false;
+    }
 }
